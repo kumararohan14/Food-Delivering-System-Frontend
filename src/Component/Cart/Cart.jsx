a704ce5a5fbeb14915fb0413886982c5f21e9b07
kumararohan14 <kumararohan14@gmail.com>
2025-12-02 11:19:02 +0530

    finished Bill Details Ui part

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -7,11 +7,36 @@ function Cart() {
     <div>
       <main className="lg:flex justify-between">
         <section className="lg:w-[30%] space-y-6 lg:min-h-screen pt-10">
-            <CartItem />
-            <CartItem />
+          <CartItem />
+          <CartItem />
+          <div className="billlDetails px-5 text-sm">
+            <p className="font-extralight py-5">Bill Details</p>
+            <div className="space-y-3">
+              <div className="flex justify-between text-gray-400">
+                <p>Item Total</p>
+                <p>Rs500</p>
+              </div>
+              <div className="flex justify-between text-gray-400">
+                <p>Deliver Fee</p>
+                <p>Rs300</p>
+              </div>
+              <div className="flex justify-between text-gray-400">
+                <p>GST and Restaurant Charges</p>
+                <p>Rs100</p>
+              </div>
+              <Divider />
+            </div>
+            <div className="flex justify-between text-gray-400">
+              <p>Total pay</p>
+              <p>Rs2000</p>
+            </div>
+          </div>
         </section>
 
-        <Divider />
+        <Divider orientation="vertical" flexItem/>
+        <section className="lg:w-[70%]">
+
+        </section>
       </main>
     </div>
   );
